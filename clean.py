Correct for poor cleaning in Petrosian Radius calculation

clean.py
@@ -70,6 +70,17 @@ def savedata(mode, hdr, galnumber, data=[], names=[]):
 
     return datacube
 
+def clean_directory(outdir):
+    if os.path.isfile(outdir+"*bright*.fits"):
+        os.system("rm "+outdir+"*bright*.fits")
+
+    if os.path.isfile(outdir+"*faint*.fits"):
+        os.system("rm "+outdir+"*faint*.fits")
+
+    if os.path.isfile(outdir+"*smooth*.fits"):
+        os.system("rm "+outdir+"*smooth*.fits")
+
+
 def clean_frame(image, outdir, sep=17.):
     '''
     Gonna try a two-stage method of cleaning:
@@ -262,7 +273,7 @@ def clean_frame(image, outdir, sep=17.):
                             names=['BSEG', 'FSEG', 'CAT'])
 
     elif mode == 'FAINT':
-        datacube = savedata(mode, ihdr, FIndex, data=[bseg, fseg, fcat], 
+        datacube = savedata(mode, ihdr, BIndex, data=[bseg, fseg, bcat], 
                             names=['BSEG', 'FSEG', 'CAT'])
 
     elif mode == 'FAINT2':
@@ -278,8 +289,8 @@ def clean_frame(image, outdir, sep=17.):
 
         
     # SAVE ALL PRODUCTS TO DATA CUBE
-    datacube.insert(0, fits.ImageHDU(data=img, header=ihdr, name='ORG'))
     datacube.insert(0, fits.ImageHDU(data=cln, header=ihdr, name='CLN'))
+    datacube.insert(1, fits.ImageHDU(data=img, header=ihdr, name='ORG'))
 
     newthing = fits.HDUList()
     for thing in datacube: 
@@ -290,7 +301,7 @@ def clean_frame(image, outdir, sep=17.):
 
 
     # Now that we've done the cleaning -- Let's test it!    
-    run_sextractor.run_SE(outdir+'f_'+basename+'.fits[1]', 'FAINT', 
+    run_sextractor.run_SE(outdir+'f_'+basename+'.fits', 'FAINT', 
                           outdir, outstr2='test')
     tseg = fits.getdata(outdir+'f_'+basename+'_faint_test_seg.fits')
     tcat = fits.getdata(outdir+'f_'+basename+'_faint_test_cat.fits')
@@ -307,16 +318,12 @@ def clean_frame(image, outdir, sep=17.):
 
     uFlag = 0
     # If we find large objs far from the center then we didn't clean enough
-    if (np.any(dist[1::] > 2*sep)) & (np.any(tarea[1::] > 100.)):
+    if (np.any(dist[1::] > 3*sep)) & (np.any(tarea[1::] > 100.)):       
         print 'UNDER CLEANED!!'
         uFlag = 1
 
-
     # clean up directory
-    os.system("rm "+outdir+"*bright*.fits")
-    os.system("rm "+outdir+"*faint*.fits")
-    os.system("rm "+outdir+"*smooth*.fits")
+    clean_directory(outdir)
 
     #FIndex, Fdist, Bdist, DIST, Farea, Barea,
     return  [category, oFlag, uFlag]
-

galaxy_plot.py
@@ -2,11 +2,15 @@
 
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
+import matplotlib.patches as mpatches
 import numpy as np
 import pyfits as fits
 from photutils import EllipticalAperture, EllipticalAnnulus
+from skimage import measure
+
 import galaxy
 import pdb
+import utils
 
     
 '''
@@ -27,13 +31,48 @@ def petro_radius(gal, image):
     '''
     Show the galaxy with a 1 Petrosian Radius elliptical aperture
     '''
-    an = EllipticalAperture((gal.x, gal.y), gal.rp, gal.rp/gal.e, gal.theta)
-    plt.figure()
-    an.plot(color='blue', lw=1.5, alpha=0.5)
+    shape = [image.shape[0]/2., image.shape[1]/2.]
+    size = 2*gal.Rp
+    aper = EllipticalAperture((gal.x, gal.y), gal.Rp, gal.Rp/gal.e, gal.theta)
+
+    fig = plt.figure(figsize=(8,8))
+    gs = plt.GridSpec(3,3)
+    ax = fig.add_subplot(gs[:,:])
+    plt.setp(ax, xlim=(shape[0]-size, shape[0]+size),
+             ylim=(shape[1]-size, shape[1]+size))
+
     imgplot = plt.imshow(image, cmap='gray_r', origin='lower')
-    imgplot.set_clim(-0.009, 0.022)
-    plt.title('1 Petrosian Radius')        
+    imgplot.set_clim(gal.med-.5*gal.rms, gal.med+10*gal.rms)
+    aper.plot(color='blue', lw=1.5, alpha=0.5)
+
+    plt.title('1 Petrosian Radius') 
+    gs.tight_layout(fig)
     plt.savefig('output/figures/'+gal.name+'_RpAper.png')
+    plt.close()
+
+def petro_radius2(params, image):
+    '''
+    Show the galaxy with a 1 Petrosian Radius elliptical aperture
+    '''
+    shape = [image.shape[0]/2., image.shape[1]/2.]
+    size = 2*params['Rp']
+    aper = EllipticalAperture((params['x'], params['y']), params['Rp'], 
+                              params['Rp']/params['e'], params['theta'])
+
+    fig = plt.figure(figsize=(8,8))
+    gs = plt.GridSpec(3,3)
+    ax = fig.add_subplot(gs[:,:])
+    plt.setp(ax, xlim=(shape[0]-size, shape[0]+size),
+             ylim=(shape[1]-size, shape[1]+size))
+
+    imgplot = plt.imshow(image, cmap='gray_r', origin='lower')
+    imgplot.set_clim(params['med']-.5*params['rms'], 
+                     params['med']+10*params['rms'])
+    aper.plot(color='blue', lw=1.5, alpha=0.5)
+
+    plt.title('1 Petrosian Radius') 
+    gs.tight_layout(fig)
+    plt.savefig('output/figures/'+params['name']+'_RpAper.png')
     plt.close()
     
     
@@ -80,23 +119,102 @@ def petro_SB(gal):
     #ax2.legend()
 
     gs.tight_layout(fig)
-
     plt.savefig('output/figures/'+gal.name+'_SBprofile.png')
     plt.close()
     
-    plt.show()
+    #plt.show()
 
+def petro_SB2(gal):
+    '''
+    Plot the SB as a function of radius
+    Plot the ratio SB/<SB> as a function of radius
+    Include the interpolation
+    '''
 
+    sb = gal['_sb']
+    avgsb = gal['_avgsb']
+    radii = gal['_rads']
+    #try:
+    interp_v = gal['_interpvals']
+    interp_r = gal['_interprads']
+    
+    xlims = [np.min(radii), np.max(radii)]
+
+    fig = plt.figure(figsize=(9,7))
+    gs = plt.GridSpec(2,1)
+    
+    ax2 = fig.add_subplot(gs[1])
+    ax1 = fig.add_subplot(gs[0], sharex=ax2)
+    plt.setp(ax1.get_xticklabels(), visible=False)
+
+    ax2.set_xlabel('Radius (pixels)', fontsize=16)
+    ax2.set_xlim(xlims[0], xlims[1])
+    ax1.set_xlim(xlims[0], xlims[1])
+
+    ax1.semilogx(radii, sb, 'ro', label='SB')
+    #ax1.errorbar(radii, sb, yerr=gal._sberr, fmt=None)
+    ax1.semilogx(radii, avgsb, 'go', label='<SB>')
+    ax1.hlines(0., 1., np.max(radii), linestyle='--')
+    ax1.set_ylim(-0.005, np.max(sb)+0.2*np.max(sb))
+    ax1.set_ylabel(r'$\mu$(R)', fontsize=16)
+
+    ax2.semilogx(radii, sb/avgsb, 'ro', label='SB/<SB>')
+    #ax2.errorbar(radii, sb/avgsb, yerr=gal._ratio_err, fmt=None)
+    ax2.semilogx(interp_r, interp_v, 'k', label='Interpolation')
+    ax2.hlines(0.2, 1., np.max(radii), linestyle='--')
+    ax2.set_ylim(-0.05, 1.05)
+    ax2.set_ylabel(r'$\mu$(R)/<$\mu$(<R)>', fontsize=16)
+    #ax2.legend()
+
+    gs.tight_layout(fig)
+    plt.savefig('output/figures/'+gal['name']+'_SBprofile.png')
+    plt.close()
+    
+    #plt.show()
 
 def asym_plot(gal, image):
+    shape = [image.shape[0]/2., image.shape[1]/2.]
+    size = 2*gal.Rp
 
-    #an = EllipticalAperture((gal.x, gal.y), gal.rp, gal.rp/gal.e, gal.theta)
+    aper = EllipticalAperture((gal.Acx, gal.Acy), gal.Rp, gal.Rp/gal.e, 
+                              gal.theta)
 
     # read in residual figure created during asymmetry calculation
     residual = fits.getdata('output/asymimgs/'+gal.name+'_res.fits')
+   
+    hist, bins = np.histogram(residual[shape[0]-size:shape[0]+size, 
+                                       shape[1]-size:shape[1]+size])
+    '''
+    pdb.set_trace()
+    plt.bar(bins, hist, width=np.mean(np.diff(bins)))
+    plt.show()
+    pdb.set_trace()
+    '''
 
+    font = {'fontname':'Helvetica'}
+    fig = plt.figure(figsize=(8,8))
+    gs = plt.GridSpec(3,3)
+    ax = fig.add_subplot(gs[:,:])
+    plt.setp(ax, xlim=(shape[0]-size, shape[0]+size),
+             ylim=(shape[1]-size, shape[1]+size))
+    plt.rc('font', family='serif')
+
+    imgplot = ax.imshow(residual, cmap='gray_r', origin='center')
+    imgplot.set_clim(bins[0], bins[-1])
+    ax.plot(gal.Acx, gal.Acy, 'b+', mew=2, ms=20)
+    aper.plot(lw=2)
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+    ax.text(.05, .05, 'A = %1.3f'%gal.A, fontsize=30, color='w', 
+            transform=ax.transAxes)
+    gs.tight_layout(fig)
+    plt.savefig('output/figures/'+gal.name+'_asym.png')
+    #plt.show()   
+    plt.close()
+
+def conc_plot(gal, image):
     shape = [image.shape[0]/2., image.shape[1]/2.]
-    size = 2*gal.rp
+    size = 1.5*gal.Rp
 
     fig = plt.figure(figsize=(8,8))
     gs = plt.GridSpec(3,3)
@@ -104,86 +222,86 @@ def asym_plot(gal, image):
     plt.setp(ax, xlim=(shape[0]-size, shape[0]+size),
              ylim=(shape[1]-size, shape[1]+size))
 
-    imgplot = ax.imshow(residual, cmap='gray_r', origin='center')
-    imgplot.set_clim(gal.med-gal.rms, gal.med+10*gal.rms)
-    ax.plot(gal.Ac[0], gal.Ac[1], 'b+', mew=2, ms=20)
-    ax.plot(gal.Ac[1], gal.Ac[0], 'r+', mew=2, ms=10)
-    ax.xaxis.set_major_formatter(plt.NullFormatter())
-    ax.yaxis.set_major_formatter(plt.NullFormatter())
-    ax.text(left, 0.1*bottom, 'A = %f3'%gal.A, fontsize=16, color='k', 
-            transform=ax.transAxes)
-    gs.tight_layout(fig)
-    plt.savefig('output/figures/'+gal.name+'_asym.png')
-
-    plt.show()
-
-    pdb.set_trace()
-
-
-    #plt.plot(galcenter[1], galcenter[0], 'k+', mew=2, ms=10)
-    # plt.plot(imgcenter[1], imgcenter[0], 'r+', mew=2, ms=10)
-    #plt.xlim(low, high)
-    #plt.ylim(low, high)
-    plt.title('Shifted Cleaned Image')
-    plt.savefig('output/asyms/'+self.name+'_asym1_CS.png')
-    plt.close()
-    
-    plt.figure()
-    plt.imshow(residual)
-    plt.plot(galcenter[1], galcenter[0], 'k+', mew=2, ms=10, 
-             label="Asymmetry Center")
-    plt.plot(imgcenter[1], imgcenter[0], 'r+', mew=2, ms=10, 
-             label="Image Center")
-    aperture.plot()
-    #plt.xlim(low, high)
-    #plt.ylim(low, high)
-    plt.title('Asymmetry Residuals (I - I180)')
-    plt.legend()
-    plt.close()
-    return 0.
-    
-
-def conc():
-    stuff
-
-    #pdb.set_trace()
-    
-    plt.figure()
-    plt.plot(x, ratio, 'ro')
-    plt.axhline(y=0.2, linestyle='--', color='k')
-    plt.axhline(y=0.8, linestyle='--', color='k')
-    plt.ylim(0., 1.4)
-    plt.title('Cumulative Flux / Total Flux vs. Radius')
-    plt.xlabel('Radius [pixel]')
-    plt.ylabel('Flux ratio [counts]')
-    #plt.show()
-    #pdb.set_trace()
-    plt.plot(r, ratios)
-    #plt.savefig('output/conc/'+self.name+'_c_conc.png')
-    plt.close()
-
-def m20_plot():
-
-    m20_ap2 = EllipticalAperture((xc, yc), self.rp, self.rp/self.e, 
-                                 self.theta)
-    contours = measure.find_contours(clean_dat, m20_galpix.min())
-    for n, contour in enumerate(contours):
-        ax.plot(contour[:,1], contour[:,0], linewidth=2)
-    pdb.set_trace()
-    m20_ap2.plot()
-    plt.plot(center[0], center[1], 'r+', mew=2, ms=10)
-    ax.set_xlim(shape[0]/2.-3*self.rp, shape[0]/2.+3*self.rp)
-    ax.set_ylim(shape[1]/2.-3*self.rp, shape[1]/2.+3*self.rp)
-    plt.savefig('output/m20/'+self.name+'_m20.pdf')
-    plt.show()
-
-    fig, ax = plt.subplots()
-    
     imgplot = ax.imshow(image, cmap='gray_r')
-    imgplot.set_clim(-0.01, 0.03)
-    
+    imgplot.set_clim(gal.med-.5*gal.rms, gal.med+10*gal.rms)
+    patches = [mpatches.Circle((gal.Acx, gal.Acy), gal.r20, fill=None,
+                               color='black', lw=2), 
+               mpatches.Circle((gal.Acx, gal.Acy), gal.r80, fill=None,
+                               color='black', lw=2)]
+    for patch in patches:
+        ax.add_patch(patch)
+    ax.text(.05, .05, 'C = %1.3f'%gal.C, fontsize=20, color='k', 
+            transform=ax.transAxes)
+
+    gs.tight_layout(fig)
+    plt.savefig('output/figures/'+gal.name+'_conc.png')
+    #plt.show()
     plt.close()
-    
+
+def m20_plot(gal, image):
+    '''
+    Plot the 2 different methods for calculating M20:
+    1. Galaxy pixels defined by 1 petrosian radius mask
+    2. Galaxy pixels defined by SB cut
+    '''
+    aperture = utils.EllipticalAperture((gal.Mcx1, gal.Mcy1), 
+                                         gal.Rp, gal.Rp/gal.e, 
+                                         gal.theta, image)
+    contours1 = measure.find_contours(image, gal._Mlevel1)
+    contours2 = measure.find_contours(image, gal._Mlevel2)
+
+    seg = fits.getdata('output/gini/'+gal.name+'_mask.fits')
+    contours3 = measure.find_contours(seg, gal.Rp_SB)
+
+    shape = [image.shape[0]/2., image.shape[1]/2.]
+    size = 2*gal.Rp
+
+    fig = plt.figure(figsize=(10,6))
+    gs = plt.GridSpec(3,4)
+
+    ax1 = fig.add_subplot(gs[:,:2])
+    plt.setp(ax1, xlim=(shape[0]-size, shape[0]+size),
+             ylim=(shape[1]-size, shape[1]+size))
+
+    imgplot = ax1.imshow(image, cmap='gray_r', origin='center')
+    imgplot.set_clim(gal.Rp_SB, gal.Rp_SB+10*gal.Rp_SB)
+    for n, contour in enumerate(contours1):
+        ax1.plot(contour[:,1], contour[:,0], color='blue', linewidth=2)
+    aperture.plot(linewidth=2, color='black')
+    ax1.plot(gal.Mcx1, gal.Mcy1, 'r+', mew=2, ms=20)
+    ax1.text(.05, .05, 'M = %1.3f'%gal.M1, fontsize=20, color='k', 
+            transform=ax1.transAxes)
+    ax1.text(.05, .12, 'G = %1.5f'%gal.G1, fontsize=20, color='k', 
+            transform=ax1.transAxes)
+    ax1.xaxis.set_major_formatter(plt.NullFormatter())
+    ax1.yaxis.set_major_formatter(plt.NullFormatter())
+    ax1.set_title('M20: Elliptical Aperture')
+
+
+    ax2 = fig.add_subplot(gs[:,2:4])
+    plt.setp(ax2, xlim=(shape[0]-size, shape[0]+size),
+             ylim=(shape[1]-size, shape[1]+size))
+
+    imgplot = ax2.imshow(image, cmap='gray_r', origin='center')
+    imgplot.set_clim(gal.Rp_SB, gal.Rp_SB+10*gal.Rp_SB)
+    for n, contour in enumerate(contours2):
+        ax2.plot(contour[:,1], contour[:,0], color='blue', linewidth=2)
+    for n, contour in enumerate(contours3):
+        ax2.plot(contour[:,1], contour[:,0], color='black', linewidth=2)
+    ax2.plot(gal.Mcx2, gal.Mcy2, 'r+', mew=2, ms=20)
+    ax2.text(.05, .05, 'M = %1.3f'%gal.M2, fontsize=20, color='k', 
+            transform=ax2.transAxes)
+    ax2.text(.05, .12, 'G = %1.5f'%gal.G2, fontsize=20, color='k', 
+            transform=ax2.transAxes)
+    ax2.xaxis.set_major_formatter(plt.NullFormatter())
+    ax2.yaxis.set_major_formatter(plt.NullFormatter())
+    ax2.set_title('M20: SB @ 1Rp on Gaussian-smoothed Image')
+
+    gs.tight_layout(fig)
+
+    plt.savefig('output/figures/'+gal.name+'_M20.png')
+    #plt.show()
+    plt.close()
 
 def plot(galaxy, hdulist):
     clean_img = hdulist['CLN'].data
@@ -191,3 +309,5 @@ def plot(galaxy, hdulist):
     petro_radius(galaxy, clean_img)
     petro_SB(galaxy)
     asym_plot(galaxy, clean_img)
+    conc_plot(galaxy, clean_img)
+    m20_plot(galaxy, clean_img)

randompy.py
@@ -3,6 +3,7 @@
 
 import os
 import string
+import argparse
 import pyfits as fits
 import numpy as np
 from astropy.table import Table
@@ -572,36 +573,97 @@ def analyse_cleaning():
         f.write('output/datacube2/f_%s\n'%n['name'])
     f.close()
     
-    
 
-
-    exit()
     
 def main():
-
-    zestdata = 'bigsample_zest.fits'
-    dataset = 'bigsample_mycat_v13.fits'
+    
+    parser = argparse.ArgumentParser(description='Compare my catalog with ZEST\nPlots Concentration, Asymmetry, Gini, and M20 of the two catalogs')
+    parser.add_argument('catalog1', type=str, 
+        help='ZEST catalog name')
+    parser.add_argument('catalog2', type=str,
+        help='My catalog name')
+    args = parser.parse_args()
 
     #comparecatalogs()
     #plotzestcassatamine()
-    
     #selectnewsample()
     #plotpetrorad_largesample()
-    
     #plotasymmetry()
     #plotconcentration()
-
     #testbackground()
-    
     #plotgini(zestdata, dataset)
-
     #plotM20(zestdata, dataset)
-
-    analyse_cleaning()
+    #analyse_cleaning()
     #pdb.set_trace()
+
+    z = Table.read(args.catalog1, format='fits')
+    m = Table.read(args.catalog2, format='ascii', delimiter=' ')
+
+    names = ['cat', 'oflag', 'uflag', 'ra', 'dec', 'e', 'x', 'y', 'a', 
+             'b', 'theta', 'elipt', 'kron', 'Rp', 'Rpflag', 'Rp_SB', 
+             'r20', 'r80', 'C', 'A', 'G1', 'G2', 'M1', 'M2']
+    names2 = ['Ac', 'Mc1', 'Mc2', 'med', 'rms' ]
+
+    # rename the columns cuz I messed up saving the table
+    for i,n in enumerate(names):
+        m.rename_column('col'+str(i+1), n)
+
+    fig = plt.figure(figsize=(10,8))
+    gs = plt.GridSpec(2,3)
+
+    cut = np.where(m['uflag'] != 0)
+
+    pdb.set_trace()
+
+    # plot Petrosian Radius
+    ax1 = fig.add_subplot(gs[0,0])
+    ax1.plot(z['rpet'][cut], m['Rp'][cut],'ro', [0,200],[0,200], 'k--')
+    ax1.set_ylim(-5., 200.)
+    ax1.set_xlim(-5., 200.)
+    ax1.set_title('Petrosian Radius')
+
+    # plot Concentration
+    ax2 = fig.add_subplot(gs[0,1])
+    ax2.plot(z['cc'][cut], m['C'][cut], 'ro', [1,5], [1,5], 'k--')
+    ax2.set_ylim(1., 6.)
+    ax2.set_title('Concentration')
+
     
-    exit()
-    
+    # plot Asymmetry
+    ax3 = fig.add_subplot(gs[0,2])
+    ax3.plot(z['aa'][cut], m['A'][cut], 'ro', [-.2,.5], [-.2,.5], 'k')
+    ax3.vlines(0., -.3, .8, linestyle='--', color='k')
+    ax3.hlines(0., -0.2, .5, linestyle='--', color='k')
+    ax3.set_ylim(-0.3, .8)
+    ax3.set_xlim(-.2, .5)
+    ax3.set_title('Asymmetry')
+
+
+    # plot Gini 
+    ax4 = fig.add_subplot(gs[1,0])
+    ax4.plot(z['gg'][cut], m['G2'][cut], 'ro',  [0,1], [0,1], 'k--')
+    ax4.set_ylim(-0.25, 1.)
+    ax4.set_xlim(0., 1.)
+    ax4.set_title('Gini')
+
+    # plot M201
+    ax5 = fig.add_subplot(gs[1,1])
+    ax5.plot(z['m20'][cut], m['M1'][cut], 'ro', [-3.,0.], [-3.,0.], 'k--')
+    ax5.set_title('M20')
+
+    # BONUS PLOT
+    ax6 = fig.add_subplot(gs[1,2])
+    ax6.plot(z['m20'][cut], m['M2'][cut], 'ro', [-3.,0.], [-3.,0.], 'k--')
+    ax6.set_title('M20')
+
+
+    gs.tight_layout(fig)
+    plt.savefig('compare_catalogs.png')
+    plt.show()
+    plt.close()
+
+    pdb.set_trace()
+
 if __name__ == '__main__':
     main()
 

utils.py
@@ -119,6 +119,44 @@ def shift_image(data, deltax, deltay, phase=0, nthreads=1, use_numpy_fft=False,
 
 #----------------------------------------------------------------------------------#
 
+def get_SB_Mask(Rp, Rp_SB, image, outname):
+    '''
+    Used to create a mask defining pixels belonging to a galaxy based on 
+    the surface brightness at 1 Petrosian radius
+
+    Steps:
+    1. convolve cleaned galaxy image with a Gaussian with sig=Rp/5
+       (Lotz et al 2004)
+    2. measure the SB, mu, at Rp
+    3. pixels in smoothed image with flux >= mu are assigned to the mask
+    4. Return the mask
+    '''
+    conv = ndimage.gaussian_filter(image, sigma=Rp/5)
+    convimg = fits.ImageHDU(data=conv)
+    convimg.writeto('output/gini/'+outname+'_conv.fits', clobber=True)
+
+    mask = np.array([True if conv[x] >= Rp_SB else False \
+                     for x in np.ndindex(conv.shape)])
+    mask = mask.reshape(conv.shape).astype('float')
+
+    label_img, num_labels = ndimage.label(mask)
+    
+    # if there exists more than one object in the mask, we need to isolate the
+    # correct one -- our object at the center
+    if num_labels > 1:
+        mask2 = np.array([True if label_img[x] == label_img[251, 251] \
+                             else False for x in np.ndindex(conv.shape)])
+        mask2 = mask2.reshape(conv.shape).astype('float')
+
+        mm = fits.ImageHDU(data=mask2)
+        mm.writeto('output/gini/'+outname+'_mask.fits', clobber=True)
+        return mask2
+    else: 
+        mm = fits.ImageHDU(data=mask)
+        mm.writeto('output/gini/'+outname+'_mask.fits', clobber=True)
+        return mask
+
+
 class EllipticalAperture(object):
 
     def __init__(self, xycenter, a, b, theta, data):
@@ -126,40 +164,75 @@ class EllipticalAperture(object):
         self.a, self.b = a, b
         self.theta = theta
         self.data = data
-        #self.radius = radius
-        #self.skyradius = skyradius
+        #self.absval = absval
 
         self.aper = self.make_aperture()
-        #self.skyaper = self.make_aperture(self.radius,self.skyradius)
+        self.phot = self.photometry()
 
 
     def make_aperture(self):
-        u,v = [], [] 
-        for x,y in np.ndindex(self.data.shape):
-            point = (x-self.x, y-self.y)
-            cosang = np.cos(self.theta)
-            sinang = np.sin(self.theta)
-            xtemp = point[0]*cosang + point[1]*sinang
-            ytemp = -point[0]*sinang + point[1]*cosang
-            u.append(xtemp**2)
-            v.append(ytemp**2)
-            
-        ellipse1 = np.array([u0/self.a**2 + v0/self.b**2 for u0, v0 in zip(u, v)])
+        cosang = np.cos(self.theta+np.pi/2.)
+        sinang = np.sin(self.theta+np.pi/2.)
+        xprime = np.array([(self.x-x)*cosang - (self.y-y)*sinang \
+                           for x,y in np.ndindex(self.data.shape)])
+        yprime = np.array([(self.x-x)*sinang + (self.y-y)*cosang \
+                           for x,y in np.ndindex(self.data.shape)])
 
-        mask = np.array([True if ellipse1[x] <= 1 else False \
-                         for x in np.ndindex(ellipse1.shape)])
+        ellipse = np.array([u**2/self.a**2 + v**2/self.b**2 \
+                            for u,v in zip(xprime, yprime)])
+
+        mask = np.array([True if ellipse[x] <= 1 else False \
+                         for x in np.ndindex(ellipse.shape)])
         mask = mask.reshape(self.data.shape)
 
         '''
-        ellipse = Ellipse([self.xc, self.yc], self.a, self.b, self.theta)
+        ellipse = Ellipse([self.x, self.y], self.a, self.b, self.theta)
         mask = np.array([True if ellipse.contains_point([x,y]) 
                          else False for x,y in np.ndindex(self.data.shape)])
         mask = mask.reshape(self.data.shape)
-        #'''
-        #pdb.set_trace()
-
+        
+        pdb.set_trace()
+        '''
         return mask.astype('float')
-    
+
+    def plot(self, ax=None, fill=False,  **kwargs):
+        '''
+        pts = np.zeros((361,2))
+        beta = self.theta
+        sin_beta = np.sin(beta)
+        cos_beta = np.cos(beta)
+
+        alpha = np.radians(np.r_[0.:360.:1j*(361)])
+        sin_alpha = np.sin(alpha)
+        cos_alpha = np.cos(alpha)
+
+        pts[:,0] = self.x + (self.a*cos_alpha*cos_beta - 
+                             self.b*sin_alpha*sin_beta)
+        pts[:,1] = self.y + (self.a*cos_alpha*sin_beta + 
+                             self.b*sin_alpha*cos_beta)
+        '''
+        
+        kwargs['fill'] = fill
+
+        if ax is None:
+            ax = plt.gca()
+
+        theta_deg = self.theta*180./np.pi
+        patch = Ellipse((self.x, self.y), 2.*self.a, 2*self.b, 
+                        theta_deg, **kwargs)
+        ax.add_patch(patch)
+
+        #ax.plot(pts[:,0], pts[:,1], **kwargs)
+
+    def photometry(self):
+        #if self.asbval:
+        return np.sum(np.abs(self.aper * self.data))
+        #else:
+        #    return np.sum(self.aper * self.data)
+
+    def area(self):
+        return len(self.aper[self.aper != 0.])
+
     @staticmethod
     def num_pix(weights):
         return np.sum(weights)    