ee809527f84f7455664ea30e46192ee825f19f70
melaniebeck <melaniebeck81@gmail.com>
2016-02-24 12:37:32 -0600

    commenting galaxy

--- galaxy.py
+++ galaxy.py
@@ -755,22 +755,46 @@ def main():
         help='Specify the desired name for output directory.')
     args = parser.parse_args()
 
+    # Select all FITS files in the given directory
     fitsfiles = np.array(sorted(glob.glob(args.directory+'*.fits')))
     #fitsfiles = sorted(glob.glob(args.directory))
 
+    # There are a lot of useless warnings that pop up -- suppress them!
     warnings.filterwarnings('ignore', message='Overwriting existing file .*',
                             module='pyfits')
 
+    # If the proper output directory doesn't exist, create it
     utils.checkdir(args.outdir+'datacube/')
 
+    # The morphology catalog is built by going through each FITS image, 
+    # cleaning it, processing it, measuring morphological parameters, and
+    # making appropriate plots FOR EACH FITS IMAGE. The morphological 
+    # parameters are appended to a huge table which is updated after each 
+    # image. All the output (Cleaned FITS files, plots and figures) are 
+    # stored in appropriately named directories which are created on the fly
+    # and nestled within the specified subdirectory.
+    #
+    # Sometimes this shit crashes because I'm a terrible programmer.
+    # In that case, I remove the offending FITS image, putting it in the 
+    # "bad_cutouts" directory and then start up the code again.
+    #
+    # When the code is rerun, we don't want to start a new morph catalog - 
+    # we want to keep appending to the old one so we test for this...
 
+
+    # check to see if a current morphology catalog exists
+    # if so -- find where cursor should go by getting the length
     try:
         t = Table.read(args.catalog_name)
         counter = len(t)
+        # this "cursor" tells us which FITS file we should start at because, 
+        # presumably, all previous images have already been processed.
         fitsfiles = fitsfiles[counter::]
     except:
         counter = 0
 
+
+    # Now that we have our list of FITS to process...
     for idx, f in enumerate(fitsfiles): 
         basename = os.path.basename(f)
         filename = args.outdir+'datacube/f_'+basename
@@ -789,21 +813,27 @@ def main():
             print "Running", os.path.basename(f)
             hdulist = fits.open(filename, memmap=True)
 
+            # Measure galaxy morphologies
             g = Galaxy(hdulist, filename, flags, args.outdir)
 
+            # Plot galaxy figures for quality control
             #if not np.isnan(g.Rp):
             #    galaxy_plot.plot(g, hdulist)
 
 
+            # If this is the first galaxy; create a Table
             if (idx == 0) and (counter == 0):
                 t, gal_dict = g.table(init=True)
                 t.add_row(gal_dict)
+            # Otherwise, append galaxy morphologies to existing Table
             else:
                 t.add_row(g.table())
-                    
+            
+            # Close any remaining FITS files
             hdulist.close()
+            # 
             t.write(args.catalog_name, overwrite=True)
-            print counter," galaxies measured!"
+            print counter+1," galaxies measured!"
             counter+=1
             del g
         else:
