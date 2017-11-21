corrected the petrosian radius calculation

analysis/analyze_morphcat.py
@@ -4,11 +4,26 @@ morphology catalogs I create with galaxy.py
 '''
 
 import numpy as np
-from astropy.table import Table, vstack
+from astropy.table import Table, vstack, join
 from collections import OrderedDict, defaultdict
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import pdb 
+import pandas as pd
+
+import matplotlib as mpl
+mpl.rcParams.update({'font.size': 24, 
+                    'font.family': 'STIXGeneral', 
+                    'mathtext.fontset': 'stix',
+                    'xtick.labelsize':16,
+                    'ytick.labelsize':16,
+                    'xtick.major.width':2,
+                    'ytick.major.width':2,
+                    'axes.linewidth':2,
+                    'lines.linewidth':3,
+                    'legend.fontsize':18})
+
 
 def lotz_mergers(m20):
     return -0.14*m20+0.33
@@ -17,22 +32,24 @@ def lotz_separation(m20):
     return  0.14*m20+0.8
 
 def compare_parameters(dat, fout='mycat'):
-    
-    colors = 'black'
+    cm = plt.cm.get_cmap('viridis')
+    #fsmooth = "t01_smooth_or_features_a01_smooth_weighted_fraction"
+    fsmooth = "t01_smooth_or_features_a01_smooth_debiased"
+    colors = dat[fsmooth]
 
-    fig = plt.figure(figsize=(20,16))
+    fig = plt.figure(figsize=(8,8))
     gs = plt.GridSpec(4,4)
     gs.update(wspace=0.005, hspace=0.005)
 
     #'''
     ax10 = fig.add_subplot(gs[3,0])
-    ax10.scatter(dat['A'], dat['C'], color=colors, marker='.', alpha=0.25)
-    ax10.set_xlabel('A', fontsize=20)
-    ax10.set_ylabel('C', fontsize=20)
+    ax10.scatter(dat['A'], dat['C'], c=colors, marker='.', alpha=0.25, cmap=cm)
+    ax10.set_xlabel(r'$A$')
+    ax10.set_ylabel(r'$C$')
     ax10.set_ylim(0., 6.)
     ax10.set_xlim(0., .8)
-    ax10.set_xticks([0.2, 0.4, 0.6])
-    ax10.set_yticks([1,2,3,4])
+    ax10.set_xticks([0.1, 0.3, 0.5, 0.7])
+    ax10.set_yticks([1,2,3,4,5])
     #ax10.tick_params(axis='x', pad=8)
     #plt.text(0.5, 0.5, '10', fontsize=20, color='red', transform=ax10.transAxes)
     
@@ -40,30 +57,30 @@ def compare_parameters(dat, fout='mycat'):
     
     ax8 = fig.add_subplot(gs[3,1], sharey=ax10)
     plt.setp(ax8.get_yticklabels(), visible=False)
-    ax8.scatter(dat['G'], dat['C'], color=colors, marker='.', alpha=0.25)
-    ax8.set_xlabel('G', fontsize=20)
+    ax8.scatter(dat['G'], dat['C'], c=colors, marker='.', alpha=0.25, cmap=cm)
+    ax8.set_xlabel(r'$G$')
     #ax8.set_ylabel('C')
     ax8.set_xlim(0.2, 0.8)
     ax8.set_ylim(0., 6.)
-    ax8.set_xticks([0.4, 0.5, 0.6])
+    ax8.set_xticks([0.3, 0.5, 0.7])
     #plt.text(0.5, 0.5, '8', fontsize=20, color='red', transform=ax8.transAxes)
     
     ax9 = fig.add_subplot(gs[2,1], sharex=ax8)
     plt.setp(ax9.get_xticklabels(), visible=False)
-    ax9.scatter(dat['G'], dat['A'], color=colors, marker='.', alpha=0.25)
+    ax9.scatter(dat['G'], dat['A'], c=colors, marker='.', alpha=0.25, cmap=cm)
     #ax9.set_xlabel('G')
-    ax9.set_ylabel('A', fontsize=20)
+    ax9.set_ylabel(r'$A$')
     ax9.set_xlim(0.2, 0.8)
     ax9.set_ylim(0., .8)
-    ax9.set_yticks([0.2, 0.4, 0.6])
+    ax9.set_yticks([0.1, 0.3, 0.5, 0.7])
     #plt.text(0.5, 0.5, '9', fontsize=20, color='red', transform=ax9.transAxes)
     
     ################
     
     ax5 = fig.add_subplot(gs[3,2], sharey=ax10)
     plt.setp(ax5.get_yticklabels(), visible=False)
-    ax5.scatter(dat['M20'], dat['C'], color=colors, marker='.', alpha=0.25)
-    ax5.set_xlabel('M20', fontsize=20)
+    ax5.scatter(dat['M20'], dat['C'], c=colors, marker='.', alpha=0.25, cmap=cm)
+    ax5.set_xlabel(r'M$_{20}$')
     #ax5.set_ylabel('C')
     ax5.set_xlim(0., -3.5)
     ax5.set_ylim(0., 6.)
@@ -72,29 +89,29 @@ def compare_parameters(dat, fout='mycat'):
     
     ax6 = fig.add_subplot(gs[2,2], sharex=ax5, sharey=ax9)
     plt.setp((ax6.get_xticklabels(), ax6.get_yticklabels()), visible=False)
-    ax6.scatter(dat['M20'], dat['A'], color=colors, marker='.', alpha=0.25)
+    ax6.scatter(dat['M20'], dat['A'], c=colors, marker='.', alpha=0.25, cmap=cm)
     #ax6.set_xlabel('M20')
     #ax6.set_ylabel('A')
     ax6.set_xlim(0., -3.5)
-    ax6.set_ylim(0., .8)
+    ax6.set_ylim(0., .7)
     #plt.text(0.5, 0.5, '6', fontsize=20, color='red', transform=ax6.transAxes)
     
     ax7 = fig.add_subplot(gs[1,2], sharex=ax5)
     plt.setp(ax7.get_xticklabels(), visible=False)
-    ax7.scatter(dat['M20'], dat['G'], color=colors, marker='.', alpha=0.25)
+    ax7.scatter(dat['M20'], dat['G'], c=colors, marker='.', alpha=0.25, cmap=cm)
     #ax7.set_xlabel('M20')
-    ax7.set_ylabel('G', fontsize=20)
+    ax7.set_ylabel(r'$G$')
     ax7.set_xlim(0., -3.5)
     ax7.set_ylim(0.2, 0.8)
-    ax7.set_yticks([0.4, 0.5, 0.6])
+    ax7.set_yticks([0.3, 0.5, 0.7])
     #plt.text(0.5, 0.5, '7', fontsize=20, color='red', transform=ax7.transAxes)
     
     ################
     
     ax1 = fig.add_subplot(gs[3,3], sharey=ax10)
     plt.setp(ax1.get_yticklabels(), visible=False)
-    ax1.scatter(dat['elipt'], dat['C'], color=colors, marker='.', alpha=0.25)
-    ax1.set_xlabel('elipt', fontsize=20)
+    ax1.scatter(dat['elipt'], dat['C'], c=colors, marker='.', alpha=0.25, cmap=cm)
+    ax1.set_xlabel(r'$1-b/a$')
     #ax1.set_ylabel('C')
     ax1.set_ylim(0., 6.)
     ax1.set_xlim(0., .9)
@@ -103,7 +120,7 @@ def compare_parameters(dat, fout='mycat'):
     
     ax2 = fig.add_subplot(gs[2,3], sharex=ax1, sharey=ax9)
     plt.setp((ax2.get_xticklabels(), ax2.get_yticklabels()), visible=False)
-    ax2.scatter(dat['elipt'], dat['A'], color=colors, marker='.', alpha=0.25)
+    ax2.scatter(dat['elipt'], dat['A'], c=colors, marker='.', alpha=0.25, cmap=cm)
     #ax2.set_xlabel('elipt')
     #ax2.set_ylabel('A')
     ax2.set_ylim(0., .8)
@@ -112,7 +129,7 @@ def compare_parameters(dat, fout='mycat'):
     
     ax3 = fig.add_subplot(gs[1,3], sharex=ax1, sharey=ax7)
     plt.setp((ax3.get_xticklabels(),ax3.get_yticklabels()), visible=False)
-    ax3.scatter(dat['elipt'], dat['G'], color=colors, marker='.', alpha=0.25)
+    ax3.scatter(dat['elipt'], dat['G'], c=colors, marker='.', alpha=0.25, cmap=cm)
     #ax3.set_xlabel('elipt')
     #ax3.set_ylabel('G')
     ax3.set_xlim(0., .9)
@@ -121,16 +138,21 @@ def compare_parameters(dat, fout='mycat'):
     
     ax4 = fig.add_subplot(gs[0,3])
     plt.setp(ax4.get_xticklabels(), visible=False)
-    ax4.scatter(dat['elipt'], dat['M20'], color=colors, marker='.', alpha=0.25)
+    im = ax4.scatter(dat['elipt'], dat['M20'], c=colors, marker='.', alpha=0.25, cmap=cm)
     #ax4.set_xlabel('elipt')
-    ax4.set_ylabel('M20', fontsize=20)
+    ax4.set_ylabel(r'M$_{20}$')
     ax4.set_ylim(0., -3.5)
     ax4.set_xlim(0., .9)
     ax4.set_yticks([-2.5, -1.5, -0.5])
     #plt.text(0.5, 0.5, '4', fontsize=20, color='red', transform=ax4.transAxes)
 
+    # add colorbar
+    cbaxes = fig.add_axes([0.13, 0.87, 0.45, 0.02])
+    cbax = fig.colorbar(mappable=im, cax=cbaxes, orientation='horizontal')
+    cbax.ax.set_xlabel(r'$f_{\mathrm{smooth}}$')
+
     #gs.tight_layout(fig)
-    plt.savefig('morph_params_'+fout+'.png')
+    plt.savefig('morph_params_'+fout+'.png', bbox_inches='tight')
     plt.show()
     plt.close()
 
@@ -360,35 +382,55 @@ def gini_m20_compare(dat, fout):
     plt.close()
 
 def Rp_compare(dat, fout):
-    fig = plt.figure(figsize=(8,8))
+    fig = plt.figure(figsize=(8,5))
 
-    ax1 = fig.add_subplot(211)
-    lims = [0., 100., 0., 75.]
-    grid, extent, aspect = density(dat['Rp']*.396, dat['petrorad_i'], lims, 25)
+    ax1 = fig.add_subplot(111)
+    lims = [0., 80., 0., 75.]
+    grid, extent, aspect = density(dat['Rp_corr']*.396, dat['petrorad_i'], lims, 25)
     im = ax1.imshow(grid.T, origin='low', extent=lims, 
                     interpolation='nearest', norm=LogNorm(vmin=1.,vmax=15000.),
                     aspect=aspect, cmap=plt.get_cmap('Reds'))
-    cbar = fig.colorbar(im)
+
+    #divider = make_axes_locatable(ax1)
+    #cax = divider.append_axes("right", size="5%", pad=0.1)
+    cbar = plt.colorbar(im, fraction=0.035, pad=0.04)
+    cbar.set_label("Galaxy number density", fontsize=18)
+
     ax1.plot([0,100], [0,100], 'k--',lw=1.5)
-    ax1.set_xlabel('My PetroRad (elliptical) [arcsec]')
-    ax1.set_ylabel('SDSS Petrosian Radius [arcsec]')
-    ax1.set_xlim(0,100.)
+    ax1.set_xlabel(r'R$_p$ [arcsec]')
+    ax1.set_ylabel(r'SDSS R$_p$ [arcsec]')
+    ax1.set_xlim(0,80.)
     ax1.set_ylim(0,75.)
 
+    """
+    lowrp = dat[dat['Rp_corr']*0.396<=20]
+    coeff = np.polyfit(lowrp['Rp_corr']*.396, lowrp['petrorad_i'], deg=1)
+    x = np.linspace(0, 20, 100)
+    y = coeff[0]*x + coeff[1]
+    print y
+    pdb.set_trace()
+    """
+
+    """
     ax2 = fig.add_subplot(212)
     lims = [0., 100., 0., 75.]
-    grid, extent, aspect = density(dat['Rp_c']*.396, dat['petrorad_i'], lims, 25)
+    grid, extent, aspect = density(dat['Rp_old']*.396, dat['petrorad_i'], lims, 25)
     im = ax2.imshow(grid.T, origin='low', extent=lims, 
                     interpolation='nearest', norm=LogNorm(vmin=1.,vmax=15000.),
-                    aspect=aspect, cmap=plt.get_cmap('Blues'))
+                    aspect=aspect, cmap=plt.get_cmap('Reds'))
     cbar = fig.colorbar(im)
     ax2.plot([0,100], [0,100], 'k--',lw=1.5, label='1-to-1')
-    ax2.set_xlabel('My PetroRad (circular)[arcsec]')
-    ax2.set_ylabel('SDSS Petrosian Radius [arcsec]')
+    #ax2.plot(x, y, ls='-.', color='r', label='linear fit')
+    #ax2.text(67, 10, r'$y={0:.2f}x+{1:.2f}$'.format(coeff[0], coeff[1]), color='r')
+
+    ax2.set_xlabel(r'My R$_p$ (elliptical: old) [arcsec]')
+    ax2.set_ylabel(r'SDSS R$_p$ (circular) [arcsec]')
     ax2.set_xlim(0,100.)
     ax2.set_ylim(0,75.)
-    
-    plt.savefig('PetroRad_compare_'+fout+'.png')
+    ax2.legend(loc='upper left', frameon=False)    
+	"""
+
+    plt.savefig('PetroRad_compare_'+fout+'.png', bbox_inches='tight')
     plt.show()
     plt.close()
 
@@ -509,24 +551,45 @@ def remove_flags(dat):
     
 def main():
 
-    #data1 = Table.read('GZ2Photoz_ancillary_morphology_masses.fits')
-    data2 = Table.read('GZ2Specz_ancillary_morphology_masses.fits')
+    #dat = Table.read("GZ2All_ancillary_morphology_urls.fits")
+    #dat = dat[dat.colnames[:100]]
+    #dat['objid'] = dat['dr7objid']
 
-    #dat = vstack((data1, data2))
-    new_data2 = remove_flags(data2)
+    dat = Table.read("GZmain_petrorads_highzgal.csv")
+    dat['objid'] = dat['objID']
 
-    dat = new_data2
+    #morphold = Table.read("../SDSSmorphology_catalogs/92415/SDSSmorphology_full_catalog_92415.fits")
+    #morphold = morphold[['objid', 'Rp', 'Rp_c']]
+    #morphold['Rp_old'] = morphold['Rp']
 
-    fout = 'fullGZ2_noflags_density'
+    morphnew = Table.read("../SDSSmorphology_full_catalog_110817.csv")
+    #morphnew['Rp_new'] = morphnew['Rp']
+
+
+    clean_mask = ((morphnew['bflag']==0) & (morphnew['oflag']==0) & \
+                 (morphnew['uflag']==0) & (morphnew['Rpflag']==0))    
+    morphclean = morphnew[clean_mask]
+    #newdat = Table.read('../measure_morph_bin4_goodapertures_newRpdef.csv')
+
+    fulldat = join(morphclean, dat, keys='objid')
+
+    idx = np.random.choice(range(len(fulldat)), size=5000, replace=False)
+    sample = fulldat[idx]
+
+    fout = 'random_sample_fsmooth_colored_clean'
     #compare_parameters_density(dat, fout='fullGZ2_ell_noflags_density')
 
-    #compare_parameters(dat, fout='fullGZ2_ell_noflags')
+    #compare_parameters(sample, fout=fout)
+
     #elliptical_circular_compare_SDSS(dat, fout)
-    #Rp_compare(dat, fout)
+    
+    Rp_compare(fulldat, fout='cleanSample_forThesis')
+    
     #distributions(dat, fout)
-    gini_m20_compare(dat, fout)
+    #gini_m20_compare(dat, fout)
     #z_mag_rp(dat, fout)
 
+    pdb.set_trace()
 
 
 if __name__ == '__main__':

morph/galaxyPlots.py
@@ -11,6 +11,7 @@ import matplotlib.patches as mpatches
 import numpy as np
 import pyfits as fits
 from photutils import EllipticalAperture, EllipticalAnnulus
+from astropy.visualization import ZScaleInterval
 from skimage import measure
 
 import pdb
@@ -51,7 +52,7 @@ def petro_radius(gal, image):
 
     plt.title('1 Petrosian Radius') 
     gs.tight_layout(fig)
-    plt.savefig(gal._outdir+'figures/'+gal.name+'_RpAper.png')
+    plt.savefig(gal.outdir+'figures/'+gal.name+'_RpAper.png')
 
     plt.close()
 
@@ -134,12 +135,12 @@ def petro_SB(gal):
     #ax2.legend()
 
     gs.tight_layout(fig)
-    plt.savefig(gal._outdir+'figures/'+gal.name+'_SBprofile.png')
+    plt.savefig(gal.outdir+'figures/'+gal.name+'_SBprofile.png')
 
     #plt.show()
     plt.close()
     
-    #plt.show()
+
 
 def petro_SB2(gal):
     '''
@@ -190,140 +191,120 @@ def petro_SB2(gal):
     plt.close()
     
 
-def asym_plot(gal, image):
-    shape = [image.shape[0]/2., image.shape[1]/2.]
-    size = 2*gal.Rp
-
-    aper = EllipticalAperture((gal.Ax, gal.Ay), gal.Rp, gal.Rp/gal.e, 
+def asym_plot(gal, image, ax=None):
+	shape = [image.shape[0]/2., image.shape[1]/2.]
+	size = 2*gal.Rp
+	
+	aper = EllipticalAperture((gal.Ax, gal.Ay), gal.Rp, gal.Rp/gal.e, 
                               gal.theta)
 
-    # read in residual figure created during asymmetry calculation
-    residual = fits.getdata(gal._outdir+'asymimgs/'+gal.name+'_res.fits')
+	# read in residual figure created during asymmetry calculation
+	residual = fits.getdata('../'+gal.outdir+'asymimgs/'+gal.name+'_res.fits')
    
-    hist, bins = np.histogram(residual[shape[0]-size:shape[0]+size, 
-                                       shape[1]-size:shape[1]+size])
-    '''
-    pdb.set_trace()
-    plt.bar(bins, hist, width=np.mean(np.diff(bins)))
-    plt.show()
-    pdb.set_trace()
-    '''
+	#hist, bins = np.histogram(residual[shape[0]-size:shape[0]+size, 
+    #                                   shape[1]-size:shape[1]+size])
+    
+	if not ax:
+		fig = plt.figure(figsize=(8,8))
+		gs = plt.GridSpec(3,3)
+		ax = fig.add_subplot(gs[:,:])
+		plt.setp(ax, xlim=(shape[0]-size, shape[0]+size),
+                 ylim=(shape[1]-size, shape[1]+size))
+		plt.rc('font', family='serif')
 
-    font = {'fontname':'Helvetica'}
-    fig = plt.figure(figsize=(8,8))
-    gs = plt.GridSpec(3,3)
-    ax = fig.add_subplot(gs[:,:])
-    plt.setp(ax, xlim=(shape[0]-size, shape[0]+size),
-             ylim=(shape[1]-size, shape[1]+size))
-    plt.rc('font', family='serif')
+	else:
+		interval = ZScaleInterval()
+		imgplot = ax.imshow(interval(residual), cmap='gray_r', origin='center')
 
-    imgplot = ax.imshow(residual, cmap='gray_r', origin='center')
-    imgplot.set_clim(bins[0], bins[-1])
-    ax.plot(gal.Ax, gal.Ay, 'b+', mew=2, ms=20)
-    aper.plot(lw=2)
-    ax.xaxis.set_major_formatter(plt.NullFormatter())
-    ax.yaxis.set_major_formatter(plt.NullFormatter())
-    ax.text(.05, .05, 'A = %1.3f'%gal.A, fontsize=30, color='w', 
-            transform=ax.transAxes)
-    gs.tight_layout(fig)
-    plt.savefig(gal._outdir+'figures/'+gal.name+'_asym.png')
+		ax.plot(gal.Ax, gal.Ay, 'r+', mew=.5, ms=10)
+		aper.plot(lw=1)
+		ax.xaxis.set_major_formatter(plt.NullFormatter())
+		ax.yaxis.set_major_formatter(plt.NullFormatter())
+		ax.text(.05, .05, 'A = %1.3f'%gal.A, fontsize=18, color='yellow', 
+            	transform=ax.transAxes)
 
-    #plt.show()   
-    plt.close()
+	if not ax:	
+		gs.tight_layout(fig)
+		plt.savefig(gal.outdir+'figures/'+gal.name+'_asym.png')
+		#plt.show()   
+		plt.close()
 
-def conc_plot(gal, image):
-    shape = [image.shape[0]/2., image.shape[1]/2.]
-    size = 1.5*gal.Rp
+def conc_plot(gal, image, ax=None):
 
-    fig = plt.figure(figsize=(8,8))
-    gs = plt.GridSpec(3,3)
-    ax = fig.add_subplot(gs[:,:])
-    plt.setp(ax, xlim=(shape[0]-size, shape[0]+size),
-             ylim=(shape[1]-size, shape[1]+size))
+	if not ax:
+		fig = plt.figure(figsize=(8,8))
+		gs = plt.GridSpec(3,3)
+		ax = fig.add_subplot(gs[:,:])
 
-    imgplot = ax.imshow(image, cmap='gray_r')
-    imgplot.set_clim(gal.med-.5*gal.rms, gal.med+10*gal.rms)
-    patches = [mpatches.Circle((gal.Ax, gal.Ay), gal.r20, fill=None,
-                               color='black', lw=2), 
-               mpatches.Circle((gal.Ax, gal.Ay), gal.r80, fill=None,
-                               color='black', lw=2)]
-    for patch in patches:
-        ax.add_patch(patch)
-    ax.text(.05, .05, 'C = %1.3f'%gal.C, fontsize=20, color='k', 
-            transform=ax.transAxes)
+	else:
+		interval = ZScaleInterval()
+		ax.imshow(interval(image), cmap='Greys_r', interpolation='nearest', origin='lower')
 
-    gs.tight_layout(fig)
-    plt.savefig(gal._outdir+'figures/'+gal.name+'_conc.png')
 
-    #plt.show()
-    plt.close()
+		apr20 = utils.MyEllipticalAperture((gal.Ax, gal.Ay), 
+                                   		   	gal.r20, gal.r20/gal.e, 
+                                   		   	gal.theta, image)
 
-def m20_plot(gal, image):
-    '''
-    Plot the 2 different methods for calculating M20:
-    1. Galaxy pixels defined by 1 petrosian radius mask
-    2. Galaxy pixels defined by SB cut
-    '''
-    aperture = utils.EllipticalAperture((gal.Mx, gal.My), 
-                                         gal.Rp, gal.Rp/gal.e, 
-                                         gal.theta, image)
-    mask1 = aperture.aper*image
+		apr80 = utils.MyEllipticalAperture((gal.Ax, gal.Ay), 
+                                   		   	gal.r80, gal.r80/gal.e, 
+                                   		   	gal.theta, image)
 
-    contours1 = measure.find_contours(mask1, gal._Mlevel1)
+		aptot = utils.MyEllipticalAperture((gal.Ax, gal.Ay), 
+                                   		   	1.5*gal.Rp, 1.5*gal.Rp/gal.e, 
+                                   		   	gal.theta, image)
+
+		apr20.plot(linewidth=1, color='black')
+		apr80.plot(linewidth=1, color='k')
+		aptot.plot(linestyle='--', color='k')
+
+		ax.text(.05, .05, 'C = %1.3f'%gal.C, fontsize=18, 
+				color='yellow', 
+            	transform=ax.transAxes)
+		ax.xaxis.set_major_formatter(plt.NullFormatter())
+		ax.yaxis.set_major_formatter(plt.NullFormatter())
+
+	if not ax:
+		gs.tight_layout(fig)
+		plt.savefig(gal.outdir+'figures/'+gal.name+'_conc.png')
+		#plt.show()
+		plt.close()
+
+def m20_plot(gal, image, ax=None):
+	aperture = utils.MyEllipticalAperture((gal.xc, gal.yc), 
+                                   		   gal.Rp, gal.Rp/gal.e, 
+                                   		   gal.theta, image)
+	mask1 = aperture.aper*image
+	contours1 = measure.find_contours(mask1, gal.Mlevel1)
 
     #seg = fits.getdata('output/gini/'+gal.name+'_mask.fits')
     #contours3 = measure.find_contours(seg, gal.Rp_SB)
     
-    shape = [image.shape[0]/2., image.shape[1]/2.]
-    size = 2*gal.Rp
-    
-    fig = plt.figure(figsize=(10,6))
-    gs = plt.GridSpec(3,3)
-    
-    ax1 = fig.add_subplot(gs[:,:])
-    plt.setp(ax1, xlim=(shape[0]-size, shape[0]+size),
-             ylim=(shape[1]-size, shape[1]+size))
+	if not ax:
+		fig = plt.figure(figsize=(10,6))
+		gs = plt.GridSpec(3,3)
+		ax = fig.add_subplot(gs[:,:])
+	else:
+		interval = ZScaleInterval()
+		ax.imshow(interval(image), cmap='Greys_r', origin='lower', interpolation='nearest')
 
-    imgplot = ax1.imshow(image, cmap='gray_r', origin='center')
-    imgplot.set_clim(gal.Rp_SB, gal.Rp_SB+10*gal.Rp_SB)
-    for n, contour in enumerate(contours1):
-        ax1.plot(contour[:,1], contour[:,0], color='blue', linewidth=2)
-    aperture.plot(linewidth=2, color='black')
-    ax1.plot(gal.Mx, gal.My, 'r+', mew=2, ms=20)
-    ax1.text(.05, .05, 'M = %s'%"{0:.3f}".format(gal.M20), fontsize=20, 
-             color='k', transform=ax1.transAxes)
-    ax1.text(.05, .12, 'G = %s'%"{0:.3f}".format(gal.G), fontsize=20, 
-             color='k', transform=ax1.transAxes)
-    ax1.xaxis.set_major_formatter(plt.NullFormatter())
-    ax1.yaxis.set_major_formatter(plt.NullFormatter())
-    ax1.set_title('M20: Elliptical Aperture')
+		for n, contour in enumerate(contours1):
+			ax.plot(contour[:,1], contour[:,0], color='blue', linewidth=2)
+    	
+		aperture.plot(linewidth=1, color='black')
+		ax.plot(gal.Mx, gal.My, 'r+', mew=.5, ms=10)
+		ax.text(.05, .05, r"M$_{20} = $"+"{0:.2f}".format(gal.M20), fontsize=18, 
+             	color='yellow', transform=ax.transAxes)
+		ax.text(.05, .15, r"$G = ${0:.2f}".format(gal.G), fontsize=18, 
+             	color='yellow', transform=ax.transAxes)
+		ax.xaxis.set_major_formatter(plt.NullFormatter())
+		ax.yaxis.set_major_formatter(plt.NullFormatter())
 
-    '''
-    if not isinstance(mask2, int):
-        ax2 = fig.add_subplot(gs[:,2:4])
-        plt.setp(ax2, xlim=(shape[0]-size, shape[0]+size),
-                 ylim=(shape[1]-size, shape[1]+size))
-        imgplot = ax2.imshow(image, cmap='gray_r', origin='center')
-        imgplot.set_clim(gal.Rp_SB, gal.Rp_SB+10*gal.Rp_SB)
-        for n, contour in enumerate(contours2):
-            ax2.plot(contour[:,1], contour[:,0], color='blue', linewidth=2)
-        for n, contour in enumerate(contours3):
-            ax2.plot(contour[:,1], contour[:,0], color='black', linewidth=2)
-        ax2.plot(gal.Mcx2, gal.Mcy2, 'r+', mew=2, ms=20)
-        ax2.text(.05, .05, 'M = %s'%"{0:.3f}".format(gal.M2), fontsize=20, 
-                 color='k', transform=ax2.transAxes)
-        ax2.text(.05, .12, 'G = %s'%"{0:.3f}".format(gal.G2), fontsize=20, 
-                 color='k', transform=ax2.transAxes)
-        ax2.xaxis.set_major_formatter(plt.NullFormatter())
-        ax2.yaxis.set_major_formatter(plt.NullFormatter())
-        ax2.set_title('M20: SB @ 1Rp on Gaussian-smoothed Image')
-    #'''
-    gs.tight_layout(fig)
-
-    plt.savefig(gal._outdir+'figures/'+gal.name+'_M20.png')
-
-    #plt.show()
-    plt.close()
+	if not ax:
+		plt.savefig(gal.outdir+'figures/'+gal.name+'_M20.png')
+		#plt.show()
+		plt.close()
+	else:
+		return ax
 
 
 #def main(galMorph, hdulist):
@@ -334,7 +315,7 @@ def plot(galMorph, hdulist):
     except:
         clean_img = hdulist['CLN'].data
 
-    utils.checkdir(galMorph._outdir+'figures/')
+    utils.checkdir(galMorph.outdir+'figures/')
 
     petro_radius(galMorph, clean_img)
     petro_SB(galMorph)
@@ -343,5 +324,13 @@ def plot(galMorph, hdulist):
     m20_plot(galMorph, clean_img)
 
 
+def main():
+
+	df = pd.read_csv("gz2sample.csv")
+	
+
+
 #if __name__ == "__main__":
 #    pyplot()
+
+    #gs.tight_layout(fig)

morph/utils.py
@@ -135,7 +135,7 @@ def get_SB_Mask(Rp, Rp_SB, image, outname):
     3. pixels in smoothed image with flux >= mu are assigned to the mask
     4. Return the mask
     '''
-    im_center = [round(image.shape[0]/2), round(image.shape[1]/2)]
+    im_center = [int(round(image.shape[0]/2)), int(round(image.shape[1]/2))]
 
     conv = ndimage.gaussian_filter(image, sigma=Rp/5)
     if not np.any(conv > Rp_SB):
@@ -154,14 +154,18 @@ def get_SB_Mask(Rp, Rp_SB, image, outname):
     # if there exists more than one object in the mask, we need to isolate the
     # correct one -- our object at the center
     if num_labels > 1:
-        mask2 = np.array([True if label_img[x] == label_img[im_center[0], 
-                                                            im_center[1]] \
-                             else False for x in np.ndindex(conv.shape)])
-        mask2 = mask2.reshape(conv.shape).astype('float')
+        try:
+            mask2 = np.array([True if label_img[x] == \
+										label_img[im_center[0], im_center[1]] \
+                              else False for x in np.ndindex(conv.shape)])
+            mask2 = mask2.reshape(conv.shape).astype('float')
 
-        mm = fits.ImageHDU(data=mask2)
-        mm.writeto(outname+'_mask.fits', clobber=True)
-        return mask2
+            mm = fits.ImageHDU(data=mask2)
+            mm.writeto(outname+'_mask.fits', clobber=True)
+            return mask2
+        except:
+            print "You're in that weird place in get_SB_Mask..."
+            pdb.set_trace()
     else: 
         mm = fits.ImageHDU(data=mask)
         mm.writeto(outname+'_mask.fits', clobber=True)